f7f76af1613c738dce42fc42031cd0f310c4c748
MaTiUs77 <matius77@gmail.com>
2017-10-18 10:45:00 -0300

    Componente baseTable
    Categorias, Marcas, Almacenes se editar y crean con Modals

--- resources/views/component/abm/baseTable.blade.php
+++ resources/views/component/abm/baseTable.blade.php
@@ -2,13 +2,12 @@
   <div class="box-header">
     <h3 class="box-title">Lista de {{ ucfirst($resource) }}</h3>
 
-    <a class="btn btn-primary pull-right" href="{{ route($resource.'.create') }}">
+    <a href="javascript:;" class="btn btn-primary pull-right" data-toggle="modal" data-target="#modalCreate">
       <i class="fa fa-plus"></i>
       Agregar
       <span class="hidden-xs">
         {{ ucfirst($resource) }}
       </span>
-
     </a>
 
   </div>
@@ -17,8 +16,7 @@
     <table class="table table-striped">
       <tbody>
       <tr>
-        <th style="width: 180px">Acciones</th>
-        <th style="width: 10px">Id</th>
+        <th style="width: 100px">Acciones</th>
         <th>Nombre</th>
       </tr>
 
@@ -30,7 +28,6 @@
                 'item' => $item,
             ])
           </td>
-          <td>{{ $item->id }}</td>
           <td>{{ $item->nombre }}</td>
         </tr>
       @endforeach
@@ -41,13 +38,11 @@
   <!-- /.box-body -->
 </div>
 
-
 @if($items instanceof  \Illuminate\Pagination\LengthAwarePaginator)
   <div class="text-center">
     {{ $items->links() }}
   </div>
 @endif
-
 
 @if(session()->has('message'))
   <script>
@@ -60,3 +55,42 @@
 
   </script>
 @endif
+
+
+@section('footer')
+
+
+  <!-- Modal -->
+  <div id="modalCreate" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Agregar {{ ucfirst($resource) }}</h4>
+        </div>
+        <div class="modal-body">
+
+          <form role="form" method="post" action="{{ route($resource.'.store') }}" id="formCreate">
+            {{ csrf_field() }}
+
+            <input id="inputCreateNombre" class="form-control input-lg" type="text" name="name" required placeholder="Ingresar nombre de {{ ucfirst($resource) }}">
+          </form>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-success" data-dismiss="modal" onclick="$('#formCreate').submit()"><i class="fa fa-plus"></i>  Crear</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <script>
+
+    $('#modalCreate').on('shown.bs.modal', function () {
+      $('#inputCreateNombre').focus();
+    });
+
+  </script>
+@endsection
--- resources/views/component/abm/btn.blade.php
+++ resources/views/component/abm/btn.blade.php
@@ -2,6 +2,14 @@
     <button class="btn btn-primary btn-xs dropdown-toggle" type="button" data-toggle="dropdown">Acciones
         <span class="caret"></span></button>
     <ul class="dropdown-menu">
+
+        <li>
+            <a href="javascript:;" class="abmEdit" data-toggle="modal" data-target="#modalEdit" data-id="{{ $item->id }}" data-nombre="{{ $item->nombre }}">
+                <i class="fa fa-edit"></i>
+                Editar con modal
+            </a>
+        </li>
+
         <li>
             <a href="{{ route($resource.'.edit',$item->id) }}">
                 <i class="fa fa-edit"></i>
@@ -13,7 +21,7 @@
 
         <li class="divider"></li>
         <li>
-            <a href="javascript:{{ $resource.$item->id }}_delete({{$item->id}});">
+            <a href="javascript:{{ $resource }}_delete({{$item->id}});">
                 <i class="fa fa-trash"></i>
                 Eliminar
             </a>
@@ -21,25 +29,72 @@
     </ul>
 </div>
 
-<form id="form_{{ $resource.$item->id }}" method="POST" action="{{ route($resource.'.destroy',$item->id) }}">
+<form id="form_{{ $resource }}_{{ $item->id }}" method="POST" action="{{ route($resource.'.destroy',$item->id) }}">
   {{ csrf_field() }}
   <input type="hidden" name="_method" value="delete" />
 </form>
 
-<script>
-  function {{ $resource.$item->id }}_delete(id) {
-    swal({
-      title: "Confirma eliminar?",
-      text: "No podra recuperar esta informacion!",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Si, eliminar!",
-      cancelButtonText: "Cancelar",
-      closeOnConfirm: false
-    },
-    function(){
-      $('#form_{{ $resource.$item->id }}').submit();
-    });
-  }
-</script>
+@if ($loop->first)
+
+    @section('footer')
+        @parent
+        <!-- Modal -->
+        <div id="modalEdit" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Editar <span id="editId"></span></h4>
+                    </div>
+                    <div class="modal-body">
+                        <form id="formEdit" class="form-horizontal" role="form" method="post" action="#">
+
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="PUT" />
+
+                            <input id="inputEditNombre" class="form-control input-lg" type="text" name="name" required placeholder="Nombre">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="abmEditSubmit btn btn-success" data-dismiss="modal" onclick="$('#formEdit').submit();"><i class="fa fa-edit"></i> Actualizar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <script>
+            function {{ $resource }}_delete(id) {
+                swal({
+                  title: "Confirma eliminar?",
+                  text: "No podra recuperar esta informacion!",
+                  type: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#DD6B55",
+                  confirmButtonText: "Si, eliminar!",
+                  cancelButtonText: "Cancelar",
+                  closeOnConfirm: false
+                },
+                function(){
+                  $('#form_{{ $resource }}_'+id).submit();
+                });
+            }
+
+            $(document).on("click", ".abmEdit", function () {
+                var editId = $(this).data('id');
+                var editNombre = $(this).data('nombre');
+                $(".modal-header #editId").text( editId);
+                $(".modal-body #inputEditNombre").val( editNombre);
+
+                var actionUrl = '{{ url($resource) }}/'+editId;
+                console.log(actionUrl);
+                $('#formEdit').attr('action',actionUrl);
+            });
+
+            $('#modalEdit').on('shown.bs.modal', function () {
+                $('#inputEditNombre').focus();
+            });
+        </script>
+    @endsection
+@endif
