00a5e9a42feccdfab40d30346eb31cbddf0b4924
MaTiUs77 <matius77@gmail.com>
2017-11-02 12:11:46 -0300

    Quitamos Redis

--- app/Http/Controllers/Api/Api.php
+++ app/Http/Controllers/Api/Api.php
@@ -22,10 +22,10 @@ class Api extends Controller
             $resumen = new ResumenDetalle($venta, $detalles);
 
             // Cada vez que se usa este metodo del api, se va a realzar un espejo en REDIS
-            $channel = 'venta_'.$venta_id;
+/*            $channel = 'venta_'.$venta_id;
 
             Redis::set($channel, json_encode($resumen));
-            Redis::publish($channel, json_encode($resumen));
+            Redis::publish($channel, json_encode($resumen));*/
 
             return compact('resumen');
         } else {
