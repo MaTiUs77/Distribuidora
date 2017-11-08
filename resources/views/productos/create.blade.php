bfd5f84ef32cc32568c92bb2fd374cd0e74231c8
MaTiUs77 <matius77@gmail.com>
2017-11-08 20:22:56 -0300

    Fix + Select2 Productos/Add
    Me olvide subir el create :D

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -3,150 +3,152 @@
 @section('contenido')
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12">
+        <form role="form" method="post" enctype="multipart/form-data" action="{{ route('productos.store') }}" >
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            <div class="row">
                 <div class="box box-default">
-            <div class="box-header with-border">
-                <h3 class="box-title">Nuevo Producto</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="row">
-
-                    <form role="form" method="post" enctype="multipart/form-data" action="{{ route('productos.store') }}" >
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <div class="col-xs-4">
-                        <div class="form-group">
-                            <div class="thumbnail">
-                                <output id="list" style="text-align: center"></output>
-                                <div class="caption">
-                                    <div class="text-center">
-                      <span class="btn btn-default btn-file">
-                        Seleccionar imagen <input type='file' id="files" name="imagen"/>
-                      </span>
-
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Nuevo Producto</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <div class="form-group">
+                                    <div class="thumbnail">
+                                        <output id="list" style="text-align: center"></output>
+                                        <div class="caption">
+                                            <div class="text-center">
+                                              <span class="btn btn-default btn-file">
+                                                Seleccionar imagen <input type='file' id="files" name="imagen"/>
+                                              </span>
+                                            </div>
+                                        </div>
                                     </div>
+
                                 </div>
                             </div>
+                            <!-- /.col -->
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Nombre o Descripcion</label>
+                                    <input class="form-control input-xs" type="text" name="nombre" placeholder="Nombre o Descripcion" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Codigo de Barras</label>
+                                    <input class="form-control input-xs" type="text" name="codigo_de_barras" required placeholder="Codigo de barras">
+                                </div>
+                                <div class="form-group">
+                                    <label>Codigo Interno</label>
+                                    <input class="form-control input-xs" type="text" name="codigo_interno" required placeholder="Codigo interno">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Stock</label>
+                                    <input class="form-control input-xs" type="text" name="stock" required placeholder="Stock">
+                                </div>
+                                <!-- /.form-group -->
+                                <div class="form-group">
+                                    <label>Stock Minimo</label>
+                                    <input class="form-control input-xs" type="text" name="stock_minimo" required placeholder="Stock Minimo">
+                                </div>
+                                <!-- /.form-group -->
+                                <div class="form-group">
+                                    <label>Estado</label>
+                                    <select name="estado"  class="form-control input-xs" >
+                                        <option value="activo">Activo</option>
+                                        <option value="inactivo">Inactivo</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <div class="form-group">
+                                    <label>Precio Proveedor</label>
+                                    <input class="form-control input-xs" type="text" name="precio_proveedor" required placeholder="Precio Proveedor">
+                                </div>
+                            </div>
+                            <div class="col-xs-4">
+                            <div class="form-group">
+                                    <label>Aplicar Procentaje %</label>
+                                    <input class="form-control input-xs" type="text" name="aplicar_porcentaje" required placeholder="Aplicar Porcentaje">
+                                </div>
+                                </div>
+                            <div class="col-xs-4">
+                            <div class="form-group">
+                                    <label>Precio Venta</label>
+                                    <input class="form-control input-xs" type="text" name="precio_venta" required placeholder="Precio">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-success">Dar de alta el producto</button>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
 
-                        </div>
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Datos opcionales</h3>
                     </div>
-                    <!-- /.col -->
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Nombre o Descripcion</label>
-                            <input class="form-control input-xs" type="text" name="nombre" placeholder="Nombre o Descripcion" required>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label>Marca</label>
+                                    <select name="marca" class="select2 form-control">
+                                        @foreach($marcas as $marca)
+                                            <option value="{{$marca->id}}">{{$marca->nombre}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Categoria</label>
+                                    <select name="categoria" class="select2 form-control">
+                                        @foreach($categorias as $categoria)
+                                            <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label>Proveedor</label>
+                                    <select name="proveedor" class="select2 form-control">
+                                        @foreach($proveedores as $proveedor)
+                                            <option value="{{$proveedor->id}}">{{$proveedor->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Almacen</label>
+                                    <select name="almacen" class="select2 form-control">
+                                        @foreach($almacenes as $almacen)
+                                            <option value="{{$almacen->id}}">{{$almacen->nombre}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- Crear nota u observacion para el producto -->
+                            <div class="col-xs-12">
+                                <div class="form-group">
+                                    <label>Nota</label>
+                                    <textarea class="form-control"name="descripcion" placeholder="Escribe alguna nota"></textarea>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label>Codigo de Barras</label>
-                            <input class="form-control input-xs" type="text" name="codigo_de_barras" required placeholder="Codigo de barras">
-                        </div>
-                        <div class="form-group">
-                            <label>Codigo Interno</label>
-                            <input class="form-control input-xs" type="text" name="codigo_interno" required placeholder="Codigo interno">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Stock</label>
-                            <input class="form-control input-xs" type="text" name="stock" required placeholder="Stock">
-                        </div>
-                        <!-- /.form-group -->
-                        <div class="form-group">
-                            <label>Stock Minimo</label>
-                            <input class="form-control input-xs" type="text" name="stock_minimo" required placeholder="Stock Minimo">
-                        </div>
-                        <!-- /.form-group -->
-                        <div class="form-group">
-                            <label>Estado</label>
-                            <select name="estado"  class="form-control input-xs" >
-                                <option value="activo">Activo</option>
-                                <option value="inactivo">Inactivo</option>
-                            </select>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <div class="form-group">
-                            <label>Precio Proveedor</label>
-                            <input class="form-control input-xs" type="text" name="precio_proveedor" required placeholder="Precio Proveedor">
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                    <div class="form-group">
-                            <label>Aplicar Procentaje %</label>
-                            <input class="form-control input-xs" type="text" name="aplicar_porcentaje" required placeholder="Aplicar Porcentaje">
-                        </div>
-                        </div>
-                    <div class="col-xs-4">
-                    <div class="form-group">
-                            <label>Precio Venta</label>
-                            <input class="form-control input-xs" type="text" name="precio_venta" required placeholder="Precio">
-                        </div>
+                        <!-- /.row -->
                     </div>
                 </div>
-                <div class="text-center">
-                    <button class="btn btn-success">Dar de alta el producto</button>
-                </div>
-                <hr>
-                <h3>OPCIONAL</h3>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <div class="form-group">
-                            <label>Marca</label>
-                            <select name="marca" class="form-control input-xs">
-                                <option value="">- Seleccionar una marca -</option>
-                                @foreach($marcas as $marca)
-                                    <option value="{{$marca->id}}">{{$marca->nombre}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>Categoria</label>
-                            <select name="categoria" class="form-control input-xs">
-                                <option value="">- Seleccionar una categoria -</option>
-                                @foreach($categorias as $categoria)
-                                    <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group">
-                            <label>Proveedor</label>
-                            <select name="proveedor" class="form-control input-xs">
-                                <option value="">- Seleccionar un proveedor -</option>
-                                @foreach($proveedores as $proveedor)
-                                    <option value="{{$proveedor->id}}">{{$proveedor->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>Almacen</label>
-                            <select name="almacen" class="form-control input-xs">
-                                <option value="">- Seleccionar un almacen -</option>
-                                @foreach($almacenes as $almacen)
-                                    <option value="{{$almacen->id}}">{{$almacen->nombre}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group">
-                            <label>Nota</label>
-                            <input class="form-control input-xs" type="text" name="descripcion" placeholder="Escribe alguna nota">
-                        </div>
-                    </div>
-                    </form>
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.box-body -->
-        </div>
-            </div>
-        </div>
+                <!-- /.box -->
+        </form>
     </div>
 
 
@@ -213,6 +215,15 @@
 
                 var calcular_precio_venta =   (precio_proveedor_val  * aplicar_porcentaje_val / 100) + precio_proveedor_val ;
                 precio_venta.val(calcular_precio_venta);
+            });
+        });
+    </script>
+
+    <script>
+        $(function() {
+            $(".select2").select2({
+                language: "es",
+                tags: true
             });
         });
     </script>
